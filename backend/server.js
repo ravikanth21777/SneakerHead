8494d1ced03a400ec4f260cca6be2346a8666fea
SuhasUngarala <suhas.ungarala@gmail.com>
2025-05-26 17:21:29 +0530

    register api added

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,6 @@
 // server.js
+
+const User= require('./models/userModel');
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -6,6 +8,41 @@ const connectDB = require('./config/db'); // Assuming you have a db.js file for 
 require('dotenv').config();
 connectDB();
 const app = express();
+
+// ========== REGISTER ==========
+app.post("/api/auth/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (!username || !email || !password) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+
+  const existingUser = await User.findOne({ email });
+  if (existingUser) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(201).json({
+    _id: newUser._id,
+    username: newUser.username,
+    email: newUser.email,
+    token,
+  });
+});
+
 
 
 
